56fc866f69c64b0beaab647563f4d93b1e1ef53b
koukibadr <badreddinekouki1998@gmail.com>
2024-06-05 20:52:04 +0100

    Create example app for package testing

--- lib/elegant_alert_dialog.dart
+++ lib/elegant_alert_dialog.dart
@@ -1,7 +1,10 @@
-library elegant_alert_dialog;
+import 'package:flutter/widgets.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class ElegantAlertDialog extends StatelessWidget {
+  const ElegantAlertDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
